f39c9b496caf320133fa9c2c111673f1ff9b4c7e
FUAD <fuadbagiyev@email.com>
2025-02-07 00:11:55 +0400

    secret file added

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 const path = require("path");
 
-const serviceAccount = require("./kirayele-7b052-firebase-adminsdk-fbsvc-4ed9ae3b5d.json");
+const serviceAccount = require("../etc/secrets/kirayele-7b052-firebase-adminsdk-fbsvc-4ed9ae3b5d.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
